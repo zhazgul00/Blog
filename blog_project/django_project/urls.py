442c81695c569bea7cf91c2afff0f27dcc2d2d2c
zhazgul00 <jazgul09.03@gmail.com>
2023-12-20 15:47:43 +0600

    Created blog django project

---
+++ blog_project/django_project/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.urls import path, include # new
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")), 
+    path("accounts/", include("accounts.urls")),
+    path("", include("blog.urls")), 
+]
