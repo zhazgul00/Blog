8a38cbb58c5a63d5d6db23e244e42f099969843f
ZhazgulAblakeeva <125367373+zhazgul00@users.noreply.github.com>
2023-12-23 12:27:20 +0600

    Update urls.py

--- news_project/apis/urls.py
+++ news_project/apis/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import NewsAPIView
 
 urlpatterns = [
+    path("<int:pk>/", NewsApiDetail.as_view(), name="news_detail"),
     path("", NewsAPIView.as_view(), name="news_list"),
 ]
