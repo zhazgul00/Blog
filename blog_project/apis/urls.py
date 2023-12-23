a70e6a4b65c69c36bd2df714db932576ebb11c47
ZhazgulAblakeeva <125367373+zhazgul00@users.noreply.github.com>
2023-12-23 12:25:10 +0600

    Update urls.py

--- blog_project/apis/urls.py
+++ blog_project/apis/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import BlogAPIView
 
 urlpatterns = [
+    path("<int:pk>/", BlogApiDetail.as_view(), name="post_detail"),
     path("", BlogAPIView.as_view(), name="blog_list"),
 ]
